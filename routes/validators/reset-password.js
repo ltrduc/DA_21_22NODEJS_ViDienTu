cc1375ca36064144fcc7828bfe2cba545dcb6d16
Lê Trí Đức <76910943+ltrduc@users.noreply.github.com>
2022-05-30 09:59:45 +0700

    Thay đổi validation reset pass

--- routes/validators/reset-password.js
+++ routes/validators/reset-password.js
@@ -5,4 +5,7 @@ module.exports = [
     .exists().withMessage('Chưa có địa chỉ Email, Email cần được gửi với key là email!')
     .notEmpty().withMessage('Vui lòng nhập địa chỉ Email!')
     .isEmail().withMessage('Địa chỉ Email không hợp lệ!'),
+  check('phone')
+    .exists().withMessage('Chưa có số điện thoại, Số điện thoại cần được gửi với key là phone!')
+    .notEmpty().withMessage('Vui lòng nhập số điện thoại!'),
 ]
